f023d403e3b756267356e675fe2824151d29541e
김설희 <102404971+shk0625@users.noreply.github.com>
2022-07-04 08:35:23 +0900

    Update include.h

--- include.h
+++ include.h
@@ -7,3 +7,21 @@
 #include <ctype.h>
 #pragma warning(disable:4996)
 
+enum ColorType {                                        //텍스트 컬러 목록
+	BLACK,                                                //0
+	darkBLUE,                                            //1
+	DarkGreen,                                            //2
+	darkSkyBlue,                                        //3
+	DarkRed,                                            //4
+	DarkPurple,                                            //5
+	DarkYellow,                                            //6
+	GRAY,                                                //7
+	DarkGray,                                            //8
+	BLUE,                                                //9
+	GREEN,                                                //10
+	SkyBlue,                                            //11
+	RED,                                                //12
+	PURPLE,                                                //13
+	YELLOW,                                                //14
+	WHITE                                                //15
+} COLOR;
